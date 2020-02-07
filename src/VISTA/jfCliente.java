bd6f25f26a38d4e56fcb76298a55a7bc23316231
ruddypazd <ruddypazd@gmail.com>
2020-02-06 22:22:15 -0400

    dfg
    ddfgdfg

--- src/VISTA/jfCliente.java
+++ src/VISTA/jfCliente.java
@@ -9,12 +9,12 @@ package VISTA;
  *
  * @author ricardopazdemiquel
  */
-public class Index_JF extends javax.swing.JFrame {
+public class jfCliente extends javax.swing.JFrame {
 
     /**
      * Creates new form Index_JF
      */
-    public Index_JF() {
+    public jfCliente() {
         initComponents();
         this.setExtendedState(this.MAXIMIZED_BOTH);
     }
@@ -154,20 +154,21 @@ public class Index_JF extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Index_JF.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jfCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Index_JF.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jfCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Index_JF.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jfCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Index_JF.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jfCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Index_JF().setVisible(true);
+                new jfCliente().setVisible(true);
             }
         });
     }
